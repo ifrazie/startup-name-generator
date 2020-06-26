6e9be7a20d7a3805327417d0fa6bd3069fe85ec3
ifrazie <ifrazie@wgu.edu>
2020-06-26 12:52:38 -0700

    :sparkles: add saved suggestions route and logic

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,33 @@ class _RandomWordsState extends State<RandomWords> {
   final _biggerFont = TextStyle(fontSize: 18.0);
 
   void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          final tiles = _saved.map(
+              (WordPair pair) {
+                return ListTile(
+                  title: Text(
+                    pair.asPascalCase,
+                    style: _biggerFont,
+                  ),
+                );
+              },
+          );
+          final divided = ListTile.divideTiles(
+              context: context,
+              tiles: tiles,
+          ).toList();
 
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        },
+      ),
+    );
   }
 
   Widget _buildRow(WordPair pair) {
